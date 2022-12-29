e771c2a87e45239cc5efb75193a46da07fa2f69d
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-12-29 19:08:29 +0600

    add-tasks-added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,32 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+
+
+
+const uri = process.env.URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const tasksCollections = client.db('to-do-app').collection('tasks')
+
+        app.get('/bla', (req, res) => {
+            res.send('Amina ovir bouin')
+        })
+
+        app.post('/add-tasks',async (req, res) => {
+            const data = req.body
+            const result = await tasksCollections.insertOne(data)
+            res.send(result)
+        })
+    } 
+    finally{
+    }
+}
+
+run().catch(console.log)
 
 app.get('/', (req, res) => {
     res.send('server is up and runnning')
