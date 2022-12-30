50d5850b739065f7fecd6b1872b0fc9be6eeb1f1
Iktiup-Mahmud <seyam.official96@gmail.com>
2022-12-30 21:20:13 +0600

    complete-delete-added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -19,16 +19,69 @@ async function run() {
     try {
         const tasksCollections = client.db('to-do-app').collection('tasks')
 
-        app.get('/bla', (req, res) => {
-            res.send('Amina ovir bouin')
-        })
-
-        app.post('/add-tasks',async (req, res) => {
+        app.post('/add-tasks', async (req, res) => {
             const data = req.body
             const result = await tasksCollections.insertOne(data)
             res.send(result)
         })
+
+        app.get('/my-tasks', async(req, res) => {
+            const email = req.query.email
+            const query = { email: email, isCompleted: 'false' }
+            const options = { isCompleted : 'false'}
+            const result = await tasksCollections.find(query).toArray()
+            // console.log(result)
+            res.send(result)
+        })
+
+        app.get('/my-tasks-completed', async(req, res) => {
+            const email = req.query.email
+            const query = { email: email, isCompleted: 'true' }
+            const options = { isCompleted : 'true'}
+            const result = await tasksCollections.find(query).toArray()
+            // console.log(result)
+            res.send(result)
+        })
+
+        app.put('/complete-task/:id', async(req, res)=> {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    isCompleted: 'true'
+                }
+            }
+            console.log(id)
+            const result = await tasksCollections.updateOne(filter, updatedDoc, option)
+            // console.log(result)
+            res.send(result)
+        })
+
+        app.put('/incomplete-task/:id', async(req, res)=> {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const option = { upsert: true }
+            const updatedDoc = {
+                $set: {
+                    isCompleted: 'false'
+                }
+            }
+            console.log(id)
+            const result = await tasksCollections.updateOne(filter, updatedDoc, option)
+            // console.log(result)
+            res.send(result)
+        })
+
+        app.delete('/deleteTask/:id', async (req, res) => {
+            const id = req.params.id
+            const filter = { _id: ObjectId(id) }
+            const result = await tasksCollections.deleteOne(filter)
+            res.send(result)
+        })
+
     } 
+
     finally{
     }
 }
